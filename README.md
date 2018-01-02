4028419cf66e0b4e850fa51fd431544b9567386c
Philip Thomas <pthomas.v3@gmail.com>
2018-01-02 15:19:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# notebook.js
+
+`.ipynb` -> `HTML`, in `JavaScript`
+
+Render Jupyter Notebooks as HTML using JavaScript
+
+
+## Motivation
+
+I regularly use [Jupyter notebooks](https://jupyter.org/) for Python projects and often upload the results as GitHub repos or Gists.  My blog posts usually contain close to the same content as the Jupyter notebooks they're based on, so why not use the .ipynb file as the source?  This script allows me to upload a notebook, then access it and convert to HTML when my blog page loads.  That way, if push new changes to the notebook code, my blog post will automatically update as well.  
+
+## Usage
+
+At it's most basic, this code is dependancy free.  Just load the script, and pass in the notebook url and an element id:
+
+```html
+<!doctype html>
+<head>
+<!-- your blog's styling here-->
+</head>
+
+<body>
+<div id="notebook"> 
+<!--Inserts Notebook HTML here-->
+</div>
+
+<!--load nb.js script-->
+<script src="./nb.js"></script>
+
+<script>
+var id = 'notebook';
+var url = 'https://raw.githubusercontent.com/psthomas/risk-return/master/returns.ipynb';
+nb.insertNotebook(url, id);
+</script>
+
+</body>
+</html>
+```
+
+The `insertNotebook` function can also take a settings object as an argument, with these defaults:
+
+```javascript
+//Using default settings:
+var settings = {
+    'code': true,              //Include code cells
+    'markdown': true,          //Include markdown cells
+    'tables': true,            //Include html data tables
+    'images': true,            //Include .png outputs 
+    'headline': false,         //Removes the first <h1> headline, useful if page has title already
+    'tableoutline': false,     //Removes the black table outline
+    'codehighlighter': 'none', //No code highlighting. Options: 'none', 'highlightjs', 'prettyprint'
+    'mdconverter': 'default'   //Use included simple markdown converter.  Options: 'default', 'showdown'
+};
+
+var id = 'notebook';
+var url = 'https://raw.githubusercontent.com/psthomas/risk-return/master/returns.ipynb';
+nb.insertNotebook(url, id, settings);
+
+```
+
+If you want code highlighting, [highlightjs](https://github.com/isagalaev/highlight.js) and Google's [code-prettify](https://github.com/google/code-prettify) are options.  Load them via your preferred method, then pass in the correct setting.  Here's an example using `highlightjs`:
+
+```html
+<!--highlight.js-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/default.min.css">
+<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/highlight.min.js"></script>
+
+<script>
+var settings = {
+	'codehighlighter': 'highlightjs'
+}
+
+var id = 'notebook';
+var url = 'https://raw.githubusercontent.com/psthomas/risk-return/master/returns.ipynb';
+nb.insertNotebook(url, id, settings);
+</script>
+
+```
+
+The built-in markdown converter (courtesy of [mmd.js](https://github.com/p01/mmd.js/blob/master/mmd.js)) does a good job, but sometimes the markdown is too complicated.  In these cases, you can load [showdown.js](https://github.com/showdownjs/showdown) and pass it as a setting:
+
+```html
+<!--showdown.js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.min.js"></script>
+
+<script>
+var settings = {
+	'mdconverter': 'showdown'
+}
+
+var id = 'notebook';
+var url = 'https://raw.githubusercontent.com/psthomas/risk-return/master/returns.ipynb';
+nb.insertNotebook(url, id, settings);
+</script>
+
+```
+
+If you'd prefer to manipulate the raw HTML string before inserting it manually, you can pass a callback function to the `returnNotebook` function.  For example, sometimes `highlightjs` doesn't correctly identify the language I'm using, so I can set some preferences and do the highlighting myself:
+
+```javascript
+var settings = {
+	'mdconverter': 'showdown'
+}
+
+function highlightCallback(html) {
+
+	//Manipulate, then insert html
+	html = '<h1>Add a New Headline</h1>' + html;
+    var el = document.getElementById('notebook');
+    el.insertAdjacentHTML('beforeend', html);
+
+    hljs.configure({'languages':['python','javascript']});
+    hljs.initHighlighting();
+}
+
+var url = 'https://raw.githubusercontent.com/psthomas/risk-return/master/returns.ipynb';
+
+nb.returnNotebook(url, highlightCallback, settings);
+
+```
+
+Note that the HTML initially won't have code highlighting if you return it as a string with returnNotebook, regardless of the settings you pass in.  
+
+## TODO
+
+This project is in it's early stages and I've mainly built it by testing it on my own notebooks -- it's by no means comprehensive.  Here are a few TODO items:
+
+* Investigate all Jupyter cell types, all cell sources and outputs to increase coverage.  See [here](https://nbformat.readthedocs.io/en/latest/format_description.html#notebook-file-format) for cell types.    
+* Build HTML without strings, possibly dynamically inserting into DOM to load quicker  
+* Handle image types other than png 
+* Figure out how to pass in custom markdown converter functions, not just use default ones  
+* Maybe allow collapsing of cells, using reddit/hackernews style collapsing
+* See if it's possible to use the Jupyter cell "collapsed" attribute to selectively exclude (or collapse) cells 
+
+## License
+
+MIT
+ 
+  
+
---
+++ nb.js
@@ -0,0 +1,265 @@
+(function(factory) {
+
+    // Find the global object for export to both the browser and web workers.
+    var globalObject = typeof window === 'object' && window ||
+                       typeof self === 'object' && self;
+
+    // Setup highlight.js for different environments. First is Node.js or
+    // CommonJS.
+    if (typeof exports !== 'undefined') {
+        factory(exports);
+    } else if (globalObject) {
+        // Export nb globally even when using AMD for cases when this script
+        // is loaded with others that may still expect a global nb.
+        globalObject.nb = factory({});
+
+        // Finally register the global nb with AMD.
+        if (typeof define === 'function' && define.amd) {
+            define([], function() {
+                return globalObject.hljs;
+            });
+        }
+    }
+
+}(function(nb) {
+
+    function ajax(myCallback, url) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url, true);
+        req.onreadystatechange = function () {
+            if (req.readyState === 4 && req.status === 200) {
+                myCallback(req.responseText);  //This argument is the last one passed, following bound vars.
+            }
+        }
+        return req.send();
+    }
+
+    function userAjax(myCallback, userCallback, url) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url, true);
+        req.onreadystatechange = function () {
+            if (req.readyState === 4 && req.status === 200) {
+                var html = myCallback(req.responseText);  //This argument is the last one passed, following bound vars.
+                userCallback(html); //Pass the html to the user's function
+            }
+        }
+        return req.send();
+    }
+
+    //Simple markdown to html conversion
+    //Source: https://github.com/p01/mmd.js/blob/master/mmd.js
+    //MIT License: https://github.com/p01/mmd.js/blob/master/LICENSE
+    function mmd(src){
+        var h='';
+
+        function escape(t){
+            return new Option(t).innerHTML;
+        }
+        function inlineEscape(s){
+            return escape(s)
+                .replace(/!\[([^\]]*)]\(([^(]+)\)/g, '<img alt="$1" src="$2">')
+                .replace(/\[([^\]]+)]\(([^(]+)\)/g, '$1'.link('$2'))
+                .replace(/`([^`]+)`/g, '<code>$1</code>')
+                .replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')
+                .replace(/\*([^*]+)\*/g, '<em>$1</em>');
+        }
+
+        src.replace(/^\s+|\r|\s+$/g, '')
+        .replace(/\t/g, '    ')
+        .split(/\n\n+/)
+        .forEach(function(b, f, R){
+            f=b[0];
+            R=
+            {
+                '*':[/\n\* /,'<ul><li>','</li></ul>'],
+                '1':[/\n[1-9]\d*\.? /,'<ol><li>','</li></ol>'],
+                ' ':[/\n    /,'<pre><code>','</pre></code>','\n'],
+                '>':[/\n> /,'<blockquote>','</blockquote>','\n']
+            }[f];
+            h+=
+                R?R[1]+('\n'+b)
+                    .split(R[0])
+                    .slice(1)
+                    .map(R[3]?escape:inlineEscape)
+                    .join(R[3]||'</li>\n<li>')+R[2]:
+                f=='#'?'<h'+(f=b.indexOf(' '))+'>'+inlineEscape(b.slice(f+1))+'</h'+f+'>':
+                f=='<'?b:
+                '<p>'+inlineEscape(b)+'</p>';
+        });
+        return h;
+    };
+
+    function handleCode(cell, settings) {
+        var html = '';
+
+        if (settings.code) {
+            if (settings.codehighlighter === 'prettyprint') {
+                //Including Google prettyprint:
+                html += '<figure class="highlight"><pre><code class="prettyprint">' + cell.source.join('') + '</code></pre></figure>';
+            } else {
+                //Using highlight.js or no highlighting
+                html += '<figure class="highlight"><pre><code>' + cell.source.join('') + '</code></pre></figure>';
+            }
+        }
+
+        if (cell.hasOwnProperty('outputs')) {
+            for (var i = 0; i<cell.outputs.length; i++) {
+                //TODO: Investigate all cell properties to increase coverage
+                if (cell.outputs[i].hasOwnProperty('data')) {
+                    if (cell.outputs[i].data.hasOwnProperty('text/html')) {
+                        //TODO: Is this 'text/html' label only used for table output?
+                        html += settings.tables ? cell.outputs[i].data['text/html'].join(''): '';
+                    } else if (cell.outputs[i].data.hasOwnProperty('image/png')) {
+                        //Also could try if 'image' is in cell.outputs[i].data, then pass 
+                        //the image type to the code below
+                        //https://stackoverflow.com/questions/13950865
+                        var base64Data = cell.outputs[i].data['image/png'];
+                        var imageStr =  '<img src="data:image/png;base64,' + base64Data + '"/>'
+                        html += settings.images ? imageStr : '';
+                    } else if (cell.outputs[i].data.hasOwnProperty('text/plain')) {
+                        html += '<pre>' + cell.outputs[i].data['text/plain'].join('') + '</pre>';
+                    }
+                } else if (cell.outputs[i].hasOwnProperty('text')) {
+                    html += '<figure class="highlight"><pre>' + cell.outputs[i].text.join('') + '</pre></figure>';
+                }
+            }
+        }
+
+        //Replace all style tag instances?
+        // var re = new RegExp('<style>(.*?)<\/style>', 'g') 
+        // html = html.replace(re, ''); 
+        if (!settings.tableoutline) {
+            html = html.replace('border="1"', ''); //Eliminate all borders
+        }
+     
+        return html;
+    }
+
+    function handleMarkdown(cell, i, settings) {
+
+        //TODO: Originally wanted to be able to pass in a custom converter function
+        //from showdown, others, but ran into issues with object scope of function
+        if (settings.mdconverter === 'showdown' && typeof showdown !== 'undefined') {
+            var converter = new showdown.Converter();
+            var html = converter.makeHtml(cell.source.join(''));
+        } else {
+            console.log('Using internal markdown converter.')
+            var html = mmd(cell.source.join(''));
+        }
+
+        if (!settings.headline && i === 0) {
+            //var re = new RegExp('<h1>(.*?)<\/h1>');
+            //Replace only first instance, ,"g" for more 
+            var re = new RegExp('<h1(.*?)<\/h1>');
+            html = html.replace(re, '');  
+        }
+
+        return html;
+    }
+
+
+    function simpleBuildHTML(settings, src) {
+
+        var obj = JSON.parse(src);
+
+        //TODO: Think about how to build the html without using strings, 
+        //possibly dynamically inserting into DOM to load quicker:
+        //https://stackoverflow.com/questions/494143
+        var html = '';
+
+        for (var i=0; i<obj.cells.length; i++) {
+            var cell = obj.cells[i];
+            switch (cell.cell_type) {
+                case 'markdown':
+                    html += handleMarkdown(cell, i, settings); 
+                    break;
+                case 'code':
+                    html +=  handleCode(cell, settings);
+                    break;
+            }
+        }
+
+        return html;
+    }
+
+    function parseSettings(settings) {
+
+        var defaultSettings = {
+            'code': true,
+            'markdown': true,
+            'tables': true,
+            'images': true,
+            'headline': false,
+            'tableoutline': false,
+            'codehighlighter': 'none',
+            'mdconverter': 'default'
+        }; 
+
+        //Overwrite defaults
+        if (settings) {
+            var keys = Object.keys(settings);
+            for (var i=0; i<keys.length; i++) {
+                defaultSettings[keys[i]] = settings[keys[i]];
+            }
+            settings = defaultSettings;
+
+        } else {
+            var settings = defaultSettings;
+        }
+
+        return settings;
+
+    }
+
+    function insertHTML(settings, id, src) {
+
+        var html = simpleBuildHTML(settings, src);
+
+        var el = document.getElementById(id);
+        el.insertAdjacentHTML('beforeend', html);
+
+        if (settings.codehighlighter === 'highlightjs' && typeof hljs !== 'undefined') {
+            //http://highlightjs.readthedocs.io/en/latest/api.html
+            //find a way to pass language configuration in?  Or just configure in optional callback:
+            //hljs.configure({'languages':['python','javascript']});  
+            hljs.initHighlighting();
+        } else if (settings.codehighlighter === 'prettyprint' && typeof PR !== 'undefined') {
+            PR.prettyPrint();
+        }
+
+    }
+
+    function returnHTML(settings, src) {
+
+        var html = simpleBuildHTML(settings, src);
+        return html;
+    }
+
+
+    /* Public Functions */
+
+    function returnNotebook(url, userCallback, settings) {
+
+        settings = parseSettings(settings);
+
+        //Short of using an synchronous call, the only other option
+        //here is to take a callback from the user that allows them to
+        //manipulate the resulting html string as needed.  
+        userAjax(returnHTML.bind(null, settings), userCallback, url);
+
+    }
+
+    function insertNotebook(url, id, settings) {
+
+        settings = parseSettings(settings);
+        ajax(insertHTML.bind(null, settings, id), url);
+
+    }
+
+    // Set public functions:
+    nb.returnNotebook = returnNotebook;
+    nb.insertNotebook = insertNotebook;
+
+    return nb;
+
+}));
